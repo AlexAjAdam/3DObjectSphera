adb4dbe05cfebc8591e059fdc2c4d6ba18b7ac1f
Kalorina <karoli.vallo@gmail.com>
2021-03-30 15:54:09 +0200

    Zaklad, nacitanie suboru done, chyba spracovanie atd

---
+++ Sphera/Sphera/ViewerWidget.cpp
@@ -0,0 +1,128 @@
+#include   "ViewerWidget.h"
+
+ViewerWidget::ViewerWidget(QString viewerName, QSize imgSize, QWidget* parent)
+	: QWidget(parent)
+{
+	setAttribute(Qt::WA_StaticContents);
+	setMouseTracking(true);
+	name = viewerName;
+	if (imgSize != QSize(0, 0)) {
+		img = new QImage(imgSize, QImage::Format_ARGB32);
+		img->fill(Qt::white);
+		resizeWidget(img->size());
+		setPainter();
+		setDataPtr();
+	}
+}
+ViewerWidget::~ViewerWidget()
+{
+	delete painter;
+	delete img;
+}
+void ViewerWidget::resizeWidget(QSize size)
+{
+	this->resize(size);
+	this->setMinimumSize(size);
+	this->setMaximumSize(size);
+}
+
+//Image functions
+bool ViewerWidget::setImage(const QImage& inputImg)
+{
+	if (img != nullptr) {
+		delete img;
+	}
+	img = new QImage(inputImg);
+	if (!img) {
+		return false;
+	}
+	resizeWidget(img->size());
+	setPainter();
+	update();
+
+	return true;
+}
+bool ViewerWidget::isEmpty()
+{
+	if (img->size() == QSize(0, 0)) {
+		return true;
+	}
+	return false;
+}
+
+//Data function
+void ViewerWidget::setPixel(int x, int y, const QColor& color)
+{
+	if (isInside(x, y)) {
+		data[x + y * img->width()] = color.rgb();
+	}
+}
+void ViewerWidget::setPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+	if (isInside(x, y)) {
+		QColor color(r, g, b);
+		setPixel(x, y, color);
+	}
+}
+
+void ViewerWidget::clear(QColor color)
+{
+	for (size_t x = 0; x < img->width(); x++)
+	{
+		for (size_t y = 0; y < img->height(); y++)
+		{
+			setPixel(x, y, color);
+		}
+	}
+	update();
+}
+
+void ViewerWidget::loadDataFromVTKFile()
+{
+	QVector<QString> fileLines;
+	QString fileName = "octahedron.vtk";
+
+	if (fileName.isEmpty()) {
+		qDebug() << "file is empty";
+		return;
+	}
+
+	QFileInfo Finfo(fileName);
+
+	if (Finfo.suffix() == "vtk") {
+
+		QFile file(fileName);
+		if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+			return;
+
+		QTextStream in(&file);
+
+		while (!in.atEnd()) {
+			QString line = in.readLine();
+			fileLines.push_back(line);
+		}
+
+		file.close();
+	}
+
+	if (fileLines.isEmpty()) {
+		QMessageBox msgBox;
+		msgBox.setText("Data did not load.");
+		msgBox.exec();
+	}
+
+	for (int i = 0; i < fileLines.size(); i++)
+	{
+		QStringList list1 = fileLines[i].split(QLatin1Char(','));
+		//e.setLoginName(list1.at(0));
+		qDebug() << list1;
+	}
+}
+
+//Slots
+void ViewerWidget::paintEvent(QPaintEvent* event)
+{
+	QPainter painter(this);
+	QRect area = event->rect();
+	painter.drawImage(area, *img, area);
+}
